42a086c9ad8fb65b92d527dda36ba8866fa48150
Yana <denisyuk_yanina@mail.ru>
2024-10-31 13:20:50 +0300

    Добавьте файлы проекта.

---
+++ DemoWin/ClientInfo.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Demo
+{
+    partial class ClientInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CompanyNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CompanyIDTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.DirectorTextBox = new System.Windows.Forms.TextBox();
+            this.TypeTextBox = new System.Windows.Forms.TextBox();
+            this.RatingTextBox = new System.Windows.Forms.TextBox();
+            this.DiscountTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.buttonSaveInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CompanyNameTextBox
+            // 
+            this.CompanyNameTextBox.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CompanyNameTextBox.Location = new System.Drawing.Point(27, 58);
+            this.CompanyNameTextBox.Name = "CompanyNameTextBox";
+            this.CompanyNameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.CompanyNameTextBox.TabIndex = 0;
+            this.CompanyNameTextBox.TextChanged += new System.EventHandler(this.CompanyNameTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название клиента";
+            // 
+            // CompanyIDTextBox
+            // 
+            this.CompanyIDTextBox.Location = new System.Drawing.Point(167, 60);
+            this.CompanyIDTextBox.Name = "CompanyIDTextBox";
+            this.CompanyIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CompanyIDTextBox.TabIndex = 2;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(305, 60);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PhoneNumberTextBox.TabIndex = 3;
+            // 
+            // DirectorTextBox
+            // 
+            this.DirectorTextBox.Location = new System.Drawing.Point(445, 60);
+            this.DirectorTextBox.Name = "DirectorTextBox";
+            this.DirectorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DirectorTextBox.TabIndex = 4;
+            // 
+            // TypeTextBox
+            // 
+            this.TypeTextBox.Location = new System.Drawing.Point(594, 60);
+            this.TypeTextBox.Name = "TypeTextBox";
+            this.TypeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TypeTextBox.TabIndex = 5;
+            // 
+            // RatingTextBox
+            // 
+            this.RatingTextBox.Location = new System.Drawing.Point(26, 146);
+            this.RatingTextBox.Name = "RatingTextBox";
+            this.RatingTextBox.Size = new System.Drawing.Size(100, 20);
+            this.RatingTextBox.TabIndex = 6;
+            // 
+            // DiscountTextBox
+            // 
+            this.DiscountTextBox.Location = new System.Drawing.Point(169, 146);
+            this.DiscountTextBox.Name = "DiscountTextBox";
+            this.DiscountTextBox.Size = new System.Drawing.Size(98, 20);
+            this.DiscountTextBox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ID клиента";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Номер телефона";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(465, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Директор";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(631, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Тип";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Рейтинг";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(191, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Скидка";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(634, 395);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(103, 23);
+            this.exitButton.TabIndex = 14;
+            this.exitButton.Text = "Выход";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaveInfo
+            // 
+            this.buttonSaveInfo.Location = new System.Drawing.Point(518, 395);
+            this.buttonSaveInfo.Name = "buttonSaveInfo";
+            this.buttonSaveInfo.Size = new System.Drawing.Size(92, 23);
+            this.buttonSaveInfo.TabIndex = 15;
+            this.buttonSaveInfo.Text = "Сохранить";
+            this.buttonSaveInfo.UseVisualStyleBackColor = true;
+            // 
+            // ClientInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSaveInfo);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DiscountTextBox);
+            this.Controls.Add(this.RatingTextBox);
+            this.Controls.Add(this.TypeTextBox);
+            this.Controls.Add(this.DirectorTextBox);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.CompanyIDTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CompanyNameTextBox);
+            this.Name = "ClientInfo";
+            this.Load += new System.EventHandler(this.ClientInfo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CompanyNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CompanyIDTextBox;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.TextBox DirectorTextBox;
+        private System.Windows.Forms.TextBox TypeTextBox;
+        private System.Windows.Forms.TextBox RatingTextBox;
+        private System.Windows.Forms.TextBox DiscountTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button buttonSaveInfo;
+    }
+}
---
+++ DemoWin/ClientInfo.cs
@@ -0,0 +1,42 @@
+﻿using DemoLib;
+using System;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class ClientInfo : Form
+    {
+        public ClientInfo()
+        {
+            InitializeComponent();
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+        }
+
+        public void SetClient(Client client)
+        {
+            CompanyNameTextBox.Text = client.Name;
+            CompanyIDTextBox.Text = client.ID.ToString();
+            PhoneNumberTextBox.Text=client.PhoneNumber;
+            DirectorTextBox.Text=client.Director;
+            TypeTextBox.Text=client.Type;
+            RatingTextBox.Text=client.Rating.ToString();
+            DiscountTextBox.Text=client.Discount.ToString();
+        }
+
+        private void CompanyNameTextBox_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void ClientInfo_Load(object sender, System.EventArgs e)
+        {
+
+        }
+        private void exitButton_Click(object sender,EventArgs e)
+        {
+            this.Close();
+        }
+
+  
+    }
+}
